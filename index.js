d03b55145295e416c97810ec5f569c99a21021ad
weikexin <2099566778@qq.com>
2017-10-21 17:14:21 +0800

    optimization

--- index.js
+++ index.js
@@ -3,39 +3,46 @@ const rpio = require('rpio');
 rpio.open(11, rpio.OUTPUT);
 
 http.createServer((req, res) => {
-    if (req.url == '/favicon.ico'){
+    if (req.url == '/favicon.ico') {
         return;
     }
-    res.writeHead(200, {'Content-Type': 'text/html'});
+    res.writeHead(200, {'Access-Control-Allow-Origin': '*', 'Content-Type': 'text/html'});
     trigger(req.url);
     res.end('Hello')
-}).listen(8866);
+}).listen(1314);
 console.log('run ok');
 
 function turnOn() {
     rpio.write(11, rpio.HIGH);
 }
-function trunOut() {
+
+function turnOut() {
     rpio.write(11, rpio.LOW);
 }
+
 function BlingBling() {
-    let len = 0;
-    trunOn();
-    setInterval(() => {
-        if (len == 5) {
-            return;
-        }
-        trunOut();
-    },100);
+    var len = 0;
+    var timer =
+        setInterval(() => {
+            turnOn();
+            setTimeout(() => {
+                turnOut();
+            }, 500);
+            if (len == 10) {
+                clearInterval(timer)
+            }
+            len++;
+        }, 510);
 }
+
 function trigger(URL) {
-    if (URL == '/turnOn'){
+    if (URL == '/turnOn') {
         turnOn();
     }
-    if (URL == '/trunOut'){
-        trunOut();
+    if (URL == '/turnOut') {
+        turnOut();
     }
-    if (URL == '/BlingBling'){
+    if (URL == '/BlingBling') {
         BlingBling();
     }
 }
